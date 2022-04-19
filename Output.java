62b4bad324081979b13ee78b83aa7922d6982dac
mohanakrishna03 <103173853+mohanakrishna03@users.noreply.github.com>
2022-04-19 12:04:58 +0530

    Add files via upload

---
+++ Output.java
@@ -0,0 +1,12 @@
+class Output
+  {
+    public static void main(String args[])
+    {
+      int a,b,c,d;
+      a=4;
+      b=3;
+      c=3;
+      d=(b*b)-(4*a*c);
+      System.out.println("d:"+d);
+    }
+  }
