37343a390462da2be119b8808bd84270b0c876e5
mohanakrishna03 <103173853+mohanakrishna03@users.noreply.github.com>
2022-04-19 12:06:28 +0530

    Add files via upload

---
+++ Second.java
@@ -0,0 +1,12 @@
+class Second
+  {
+    public static void main(String args[])
+    {
+      int l,b,ar,pr;
+      l=50;
+      b=20;
+      ar=l*b;
+      pr=2*(l+b);
+      System.out.println("area is:"+ar+"\nperimeter is:"+pr);
+    }
+  }
