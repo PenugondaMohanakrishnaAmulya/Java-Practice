849f33e94f821993cefe4bbf4e0900b238e6a26d
mohanakrishna03 <103173853+mohanakrishna03@users.noreply.github.com>
2022-04-19 11:56:10 +0530

    Add files via upload

---
+++ Percentage.java
@@ -0,0 +1,14 @@
+class Percentage
+  {
+    public static void main(String args[])
+    {
+    int a,b,c,d,e,per;
+    a=30;
+      b=40;
+      c=20;
+      d=10;
+      e=50;
+      per=(a+b+c+d+e)*500/100;
+     System.out.println("percentage is:"+per); 
+    }
+  }
