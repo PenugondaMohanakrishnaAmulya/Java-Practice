c7460850f83032fa3e6887dedf64c08f9780220a
mohanakrishna03 <103173853+mohanakrishna03@users.noreply.github.com>
2022-04-19 12:07:05 +0530

    Add files via upload

---
+++ SI.java
@@ -0,0 +1,12 @@
+class SI
+  {
+    public static void main(String args[])
+    {
+      int p,t,r,SI;
+      p=500;
+      t=4;
+      r=2;
+      SI=(p*t*r)/100;
+      System.out.println("Amulya is:"+SI);
+    }
+  }
