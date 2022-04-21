e8b9879044f75271867bcb082546d15bde916bb1
Penugonda Mohanakrishna Amulya <103173853+PenugondaMohanakrishnaAmulya@users.noreply.github.com>
2022-04-21 16:18:44 +0530

    Add files via upload

---
+++ Circle.java
@@ -0,0 +1,47 @@
+//JFM1T3_Assignment1: 
+/* Write a program to calculate the circumference of a circle and print the result in whole number. 
+  Allow user to input the cirlce radius from terminal. 
+
+  Sample Input:
+    radius of the circle=6
+
+  Expected Output:
+   Circumference of the circle=38
+   
+
+*/ 
+
+//import statement for java program to read inputs using Scanner class 
+import java.util.Scanner;
+
+public class Circle { 
+  public static void main(String args[])
+  {
+
+//Define main method
+
+//Declare the variables
+int rad;
+double a=3.14;
+  System.out.println("radius of the circle=");
+    Scanner sc=new Scanner(System.in);
+    rad=sc.nextInt();
+    double circumference= 2*rad*a;
+    System.out.println("Circumference of the circle is="+Math.round(circumference));
+
+    }
+//Use the scanner class to provide radius at execution time
+
+/*
+Scanner scanner = new Scanner(System.in);
+System.out.println("Enter radius as a positive integer");
+radius= scanner.nextInt();
+*/
+
+//Caluculate the circumference of the circle
+
+//Casting the floating-point value to int  
+
+//Print the Result
+
+} 
